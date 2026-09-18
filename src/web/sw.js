ca3392f016314cbcb4f145ba1ee0b553b7f157cb
galaxyuniversity900-oss <galaxyuniversity900@gmail.com>
2026-09-18 10:02:44 +0300

    Add minimal PWA service worker

---
+++ src/web/sw.js
@@ -0,0 +1,1 @@
+const CACHE='wa-workspace-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.json']))));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
